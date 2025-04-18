679c77a1a6952253ecb02df634e9aa99cef06a95
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-04-18 22:44:21 +0200

    feat(api): use validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 // Usefull to store JWT TOKEN into Cookies
 import * as cookieParser from 'cookie-parser';
@@ -8,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
