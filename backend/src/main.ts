034a4e7244b079636e182c37a2692ddc93313fec
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-04-18 22:44:05 +0200

    feat(api): set a prefix to api url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
