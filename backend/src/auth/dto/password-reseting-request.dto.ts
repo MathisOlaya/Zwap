c4528ee94fe4262d50a216cea718833c47ac47c5
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-04-20 22:00:05 +0200

    feat(dto): creating and updating dto for password reseting

--- backend/src/auth/dto/password-reseting-request.dto.ts
+++ backend/src/auth/dto/password-reseting-request.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
-export class ResetPasswordDto {
+export class ResetPasswordMailDto {
   @IsEmail({}, { message: 'Merci de renseigner un email valide' })
   email: string;
 }
