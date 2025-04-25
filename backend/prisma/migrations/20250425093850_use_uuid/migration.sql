c8180078e3ee971fcf2c250a11237f668b21cf9b
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-04-25 11:53:27 +0200

    chore(db): replace id by uuid

---
+++ backend/prisma/migrations/20250425093850_use_uuid/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - The primary key for the `Token` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - Changed the type of `id` on the `Token` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `id` on the `User` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- AlterTable
+ALTER TABLE "Token" DROP CONSTRAINT "Token_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" UUID NOT NULL,
+ADD CONSTRAINT "Token_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" UUID NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
