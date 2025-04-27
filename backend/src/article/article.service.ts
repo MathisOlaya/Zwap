a13d1c2a143511ba59b0f33fdb7974fae6af4ad2
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-04-27 15:48:30 +0200

    feat(article): initializing service

--- backend/src/article/article.service.ts
+++ backend/src/article/article.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+// DTOs
+import { ArticleCreationDto } from './dto/article-creation.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Injectable()
 export class ArticleService {}
+export class ArticleService {
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly cloudinaryService: CloudinaryService,
+  ) {}
+
+}
