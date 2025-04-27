894f4d2ab064d97e07697ea522aa07658ff86855
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-04-27 15:48:46 +0200

    feat(validation): create a function for validate images

--- backend/src/article/article.service.ts
+++ backend/src/article/article.service.ts
@@ -7,11 +7,36 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Injectable()
-export class ArticleService {}
 export class ArticleService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly cloudinaryService: CloudinaryService,
   ) {}
 
+  // Image validation
+  useImageValidator(files: Array<Express.Multer.File>) {
+    if (!files.length) {
+      throw new HttpException(
+        "Merci de fournir au minimum une image de l'article",
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    if (files.length > 15) {
+      throw new HttpException(
+        "Vous avez dépassé la limite d'images par articles",
+        HttpStatus.NOT_ACCEPTABLE,
+      );
+    }
+
+    //Are they pictures ?
+    for (const file of files) {
+      if (!file.mimetype.startsWith('image/')) {
+        throw new HttpException(
+          'Seuls les images sont autorisés',
+          HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+        );
+      }
+    }
+  }
 }
