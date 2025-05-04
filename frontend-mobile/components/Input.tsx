6ea98dd40cdbffc0700de9e59aace4d62302ff54
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-04 23:02:34 +0200

    feat(component): create a component for input

---
+++ frontend-mobile/components/Input.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+
+//Responsive
+import { font, gapH, gapV, height, width } from "@/utils/responsive";
+
+// Colors
+import colors from "@/constants/Color";
+
+// Props
+type InputProps = {
+  label: string;
+  placeholder: string;
+  onChangeText: (text: string) => void;
+  secureTextEntry?: boolean;
+};
+
+const Input = (Props: InputProps) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.labelContainer}>
+        <Text style={styles.label}>{Props.label}</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder={Props.placeholder}
+        onChangeText={Props.onChangeText}
+        secureTextEntry={Props.secureTextEntry ? Props.secureTextEntry : false}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    gap: gapV(4),
+  },
+  labelContainer: {
+    paddingLeft: width(8),
+  },
+  label: {
+    color: colors.grey200,
+    fontSize: font(12),
+    fontFamily: "Poppins-Regular",
+  },
+  input: {
+    backgroundColor: colors.grey200,
+    height: height(44),
+    width: width(320),
+    borderRadius: 12,
+    paddingLeft: gapH(8),
+  },
+});
+
+export default Input;
