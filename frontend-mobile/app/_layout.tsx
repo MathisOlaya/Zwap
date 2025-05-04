5639d9f4206e255bf2b05dea61d305f983dc84d5
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-04 23:02:13 +0200

    feat(fonts): load fonts at start

--- frontend-mobile/app/_layout.tsx
+++ frontend-mobile/app/_layout.tsx
@@ -1,7 +1,27 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  // Fonts
+  const [loaded, error] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins/Poppins-Regular.ttf"),
+    "Poppins-LightItalic": require("../assets/fonts/Poppins/Poppins-LightItalic.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Stack
       screenOptions={{
