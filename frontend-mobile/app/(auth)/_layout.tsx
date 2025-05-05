d4f98ad796d98ad7999e36fc26de8e28ad427473
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-05 23:20:58 +0200

    feat(auth): redirect user to home if he's authenticated and he's trying to access auth page

--- frontend-mobile/app/(auth)/_layout.tsx
+++ frontend-mobile/app/(auth)/_layout.tsx
@@ -1,6 +1,15 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
+
+// Context
+import { useAuth } from "@/context/AuthContext";
 
 export default function AuthLayout() {
+  const { isAuthenticated } = useAuth();
+
+  if (isAuthenticated) {
+    return <Redirect href="/home" />;
+  }
+
   return (
     <Stack
       screenOptions={{
