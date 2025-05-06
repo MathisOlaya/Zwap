3e1f1af2b1af1f8e6ed61008894848bb04b32c73
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-06 23:36:29 +0200

    feat(register): define register rule

--- frontend-mobile/validation/userSchema.ts
+++ frontend-mobile/validation/userSchema.ts
@@ -1,6 +1,19 @@
 import { z } from "zod";
 
-export const loginSchema = z.object({
-  email: z.string().email("Email invalide"),
-  password: z.string().min(8, "Mot de passe trop court"),
+// Custom schema
+const phoneSchema = z.string().regex(/^(\+41|0)[1-9](\d{2}){4}$/, {
+  message: "Numéro de téléphone invalide",
+});
+
+const passwordSchema = z
+  .string()
+  .min(8, "Le mot de passe doit contenir au minimum 8 caractères")
+  .nonempty("Le mot de passe ne peut pas être vide")
+  .regex(/[A-Z]/, "Le mot de passe doit contenir au moins une majuscule");
+
+export const registerSchema = z.object({
+  firstname: z.string().nonempty("Le prénom ne peut pas être vide"),
+  phoneNumber: phoneSchema,
+  email: z.string().email("L'adresse mail fournie n'est pas valide").nonempty("L'adresse mail ne peut pas être vide"),
+  password: passwordSchema,
 });
