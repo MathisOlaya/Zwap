72ce3133a61507cd6c665bbe487696929b373baa
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-06 23:36:11 +0200

    feat(register): define register type body

--- frontend-mobile/types/user.ts
+++ frontend-mobile/types/user.ts
@@ -2,3 +2,10 @@ export type UserLoginInput = {
   email: string;
   password: string;
 };
+
+export type UserRegisterInput = {
+  firstname: string;
+  phoneNumber: string;
+  email: string;
+  password: string;
+};
