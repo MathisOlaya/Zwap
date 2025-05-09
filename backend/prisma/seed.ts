0dfc038edec718d532f38c2d50d81f586f3faefd
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-09 19:01:19 +0200

    feat(db): add all categories (+seed)

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -3,13 +3,65 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
+  const categories = [
+    'T-shirts',
+    'Chemises',
+    'Pulls & Sweats',
+    'Vestes & Manteaux',
+    'Pantalons',
+    'Shorts',
+    'Jeans',
+    'Costumes',
+    'Jogging & Survêtements',
+    'Robes',
+    'Jupes',
+    'Blouses',
+    'Bodys',
+    'Combinaisons',
+    'Baskets',
+    'Bottes & Bottines',
+    'Sandales & Tongs',
+    'Mocassins & Derbies',
+    'Talons',
+    'Chaussures de sport',
+    'Chaussures habillées',
+    'Casquettes & Bonnets',
+    'Écharpes & Foulards',
+    'Ceintures',
+    'Sacs à main',
+    'Sacs à dos',
+    'Montres',
+    'Lunettes de soleil',
+    'Bijoux',
+    'Sous-vêtements homme / femme',
+    'Chaussettes',
+    'Maillots de bain',
+    'Tenues de sport',
+    'Accessoires sportifs',
+    'Vêtements bébé',
+    'Chaussures enfants',
+    'Pyjamas enfants',
+    'Ensembles',
+    'Pyjamas',
+    'Loungewear / Homewear',
+    'Déguisements',
+  ];
+
   // Insert Token
-  const token = await prisma.token.create({
-    data: {
-      type: 'Zoho',
-      key: 'this-random-key-will-generate-itself',
-      expiresIn: 1745751793696,
-    },
+  // const token = await prisma.token.create({
+  //   data: {
+  //     type: 'Zoho',
+  //     key: 'this-random-key-will-generate-itself',
+  //     expiresIn: 1745751793696,
+  //   },
+  // });
+
+  categories.map(async (category) => {
+    await prisma.category.create({
+      data: {
+        name: category,
+      },
+    });
   });
 }
 
