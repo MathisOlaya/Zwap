3da9a204dbe5d30abd23131d9d8e709f16b16754
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-19 23:55:07 +0200

    feat(recommendation): request api for articles recommendation

--- frontend-mobile/services/HomeService.ts
+++ frontend-mobile/services/HomeService.ts
@@ -17,3 +17,27 @@ const apiClient = axios.create({
   },
 });
 
+// Get articles recommendation for user
+async function getUserArticlesRecommendation(): Promise<Article[]> {
+  try {
+    // Fetching API
+    const response = await apiClient.get("/foryou");
+
+    if (response.status === HttpStatusCode.Ok) {
+      return response.data;
+    }
+    return [];
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      // Axios error from backend
+      const message = Array.isArray(err.response?.data.message)
+        ? err.response?.data.message[0] || "Une erreur est survenue"
+        : err.response?.data.message || "Une erreur est survenue";
+
+      throw new Error(message);
+    }
+    throw new Error("Une erreur est survenue");
+  }
+}
+
+export default getUserArticlesRecommendation;
