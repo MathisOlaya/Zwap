7968d579c46f081702092876c8132f6a347d2734
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-19 23:57:56 +0200

    feat(foryou): displaying recommended articles

--- frontend-mobile/app/(tabs)/(home)/home.tsx
+++ frontend-mobile/app/(tabs)/(home)/home.tsx
@@ -6,7 +6,6 @@ import { useEffect } from "react";
 // Responsive
 import { font, width, height, gapH, gapV } from "@/utils/responsive";
 
-// Temp : Icons (then Lucid)
 // Components
 import Header from "@/components/MainPage/header";
 import ArticleDisplayer from "@/components/Articles/ArticleDisplayer";
@@ -15,7 +14,6 @@ import ArticleDisplayer from "@/components/Articles/ArticleDisplayer";
 import { useInitialData } from "@/hooks/useInitialData";
 
 export default function HomeScreen() {
-  const { signOut } = useAuth();
   // API Request
   const articlesRecommendation = useInitialData();
 
@@ -32,10 +30,9 @@ export default function HomeScreen() {
   // View
   return (
     <SafeAreaView style={styles.page}>
-      <View style={styles.header}>
-        <Text style={styles.title}>Accueil</Text>
-        <Image source={require("@/assets/icons/search.png")} />
-      </View>
+      <Header />
+      {/* ForYou Content */}
+      <ArticleDisplayer title="Pour vous" articles={articlesRecommendation} />
     </SafeAreaView>
   );
 }
@@ -44,15 +41,6 @@ const styles = StyleSheet.create({
   page: {
     paddingHorizontal: gapH(8),
     paddingVertical: gapV(12),
-  },
-  header: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingHorizontal: gapH(12),
-  },
-  title: {
-    fontSize: font(20),
-    fontFamily: "Poppins-Medium",
+    gap: gapH(12),
   },
 });
