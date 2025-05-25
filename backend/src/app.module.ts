d0538a9757a59f8842ed4a58e4aee0c087388c08
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-05-26 01:18:03 +0200

    feat: create new module for category

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,10 +9,12 @@ import { MailModule } from './mail/mail.module';
 import { ArticleModule } from './article/article.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { StripeModule } from './stripe/stripe.module';
+import { CategoryService } from './category/category.service';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [AuthModule, MailModule, ArticleModule, CloudinaryModule, StripeModule],
+  imports: [AuthModule, MailModule, ArticleModule, CloudinaryModule, StripeModule, CategoryModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService, CookiesService],
+  providers: [AppService, PrismaService, CookiesService, CategoryService],
 })
 export class AppModule {}
